3a9cdba7796298f2011732b5173e5800cc694d18
Umidjon <69582810+akbaraliyevumid@users.noreply.github.com>
2021-03-27 17:32:16 +0500

    WCF Chat
    WCF Chat on WPF

---
+++ wcf_chat/IServiceChat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))] //Etot atribut govorit ot om chto interfeys nije budet predstavlyat soboy dogovorennost o tom chto kak nash kliyent budet vzaimodeystvovat s nashim serverom.
+    public interface IServiceChat
+    {
+        [OperationContract] // v kajdom metode interfeysa dolgen bit etot atribut, to yest metod v kotorom yest etot kontrakt budet viden so storoni kliyenta.
+        int Connect(string name); // S pomoshyu etojo metoda budem podklyuchatsya k servisu.
+
+        [OperationContract]
+        void Disconnect(int id); // Etot metod budet vizivatsya kogda kliyent pokidayet chat. Nujet on nam dlya togo chto bi soobshit nashemu servisu o tom chto takogo kliyenta u nego bolshe netu.
+
+        [OperationContract(IsOneWay = true)]//Yesli ne nujno dojidatsya otveta ot servera dobavim parametr IsOneWat so znacheniyem true.
+        void SendMessage(string message, int id);//Etot metod prinimayet strokovoye soobsheniye.
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MessageCallback(string message);
+    }
+}
---
+++ wcf_chat/ServiceChat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat //v etom klasse budut realizovanni vse metodi nashego interfeysa.
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+
+            SendMessage(": " + user.Name + " is connected!", 0);
+            users.Add(user);
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user!=null)
+            {
+                users.Remove(user);
+                SendMessage(": " + user.Name + " is disconnected!", 0);
+            }
+        }
+        
+        public void SendMessage(string message, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + " ";
+                }
+
+                answer += message;
+
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MessageCallback(answer);
+            }
+        }
+    }
+}
